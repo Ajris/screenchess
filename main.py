4a94cdc27c34d02ff7ffa9e34a8dba9320b4b4e5
Ajris <hubiflower@gmail.com>
2019-12-16 22:06:51 +0100

    Small fixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import crop_image
 
 
 def main():
-    img = cv2.imread('images/board.jpg')
+    img = cv2.imread('images/input/board.jpg')
     detected_pieces_image, threshold, contours = detect_pieces.detect_edges(img)
     crop_image.find_pieces(detected_pieces_image, threshold, contours)
 
