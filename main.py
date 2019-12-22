7d3f70e549e7ff9197c8655de589aca73d233f54
Karol Szuster <kszuster@protonmail.com>
2019-12-22 18:25:40 +0100

    working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import crop_image
 import chess
 import numpy as np
+import time
 
 from recognize_chessboard import top_down_transform, find_markers
 from clusterlist import ClusterList
@@ -9,6 +10,7 @@ from clusterlist import ClusterList
 
 def main():
     img = cv2.imread('images/input/test4.jpg')
+    start = time.time()
     markers = find_markers(img)
     cluster = ClusterList(lambda x, y: np.linalg.norm(x - y) < 40)
     for point in markers:
@@ -17,6 +19,7 @@ def main():
     board = chess.Board(None)
     for position, color in crop_image.find_pieces(warped):
         board.set_piece_at(position, chess.Piece(chess.PAWN, color))
+    print(time.time() - start)
     return board.fen()
 
 
