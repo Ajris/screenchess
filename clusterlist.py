31bde31f112ead3b93b8a88554cf3510f0366428
Karol Szuster <kszuster@protonmail.com>
2020-01-05 16:48:59 +0100

    clusterlist updated

--- clusterlist.py
+++ clusterlist.py
@@ -1,15 +1,19 @@
 from collections import UserList
-from typing import Callable, Optional, List
+from typing import Callable, Optional, List, TypeVar
+
+T = TypeVar('T')
 
 
 class ClusterList(UserList):
-    def __init__(self, criterion: Callable[..., bool], initlist: Optional[List] = None) -> None:
-        super().__init__(initlist)
+    def __init__(self, criterion: Callable[..., bool], merger: Callable[[T, T], T]) -> None:
+        super().__init__()
         self.criterion = criterion
+        self.merger = merger
 
     def append(self, item) -> None:
-        for i in self.data:
-            if self.criterion(i, item):
+        for i, el in enumerate(self.data):
+            if self.criterion(el, item):
+                self.data[i] = self.merger(el, item)
                 return
         super().append(item)
 
