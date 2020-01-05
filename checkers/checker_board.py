56bcdf1354a8e9519c69ae3ececc13485c9577b8
Ajris <hubiflower@gmail.com>
2020-01-05 18:44:28 +0100

    karol

--- checkers/checker_board.py
+++ checkers/checker_board.py
@@ -1,30 +1,17 @@
 from collections import defaultdict
 
-WHITE = 1
-BLACK = 2
-
 
 class CheckerBoard:
     def __init__(self, board=None):
-        self.board = [[0 for _ in range(8)] for _ in range(8)] if board is None else board
+        self.board = [[None for _ in range(8)] for _ in range(8)] if board is None else board
 
-    def set_piece(self, x, y, piece):
-        self.board[x][y] = piece
+    def set_piece(self, x, y, color):
+        self.board[x][y] = color
 
-    def find_moves(self, piece):
+    def find_moves(self, color):
         possible_moves = defaultdict(list)
         for column in self.board:
             for row in column:
-                if self.board[column][row] == piece:
-                    if piece == WHITE:
-                        if (can_move(column + 1, row + 1)):
-                            possible_moves[(column, row)].append((column + 1, row + 1))
-                        elif can_move(column + 1, row - 1):
-                            possible_moves[(column, row)].append((column + 1, row - 1))
-                    elif piece == BLACK:
-                        if (can_move(column - 1, row + 1)):
-                            possible_moves[(column, row)].append((column - 1, row + 1))
-                        elif can_move(column - 1, row - 1):
-                            possible_moves[(column, row)].append((column - 1, row - 1))
-    def can_move(self, x, y):
-        pass
+                if self.board[column][row].color == color:
+                    moves = color.find_possible_moves(column, row, self.board)
+                    possible_moves[(column, row)].append(moves)
